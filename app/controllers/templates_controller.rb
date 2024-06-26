785495d1e0985735f95dca81d1f1e173383bf3e0
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-06-26 09:40:43 -0300

    fix: Remove redundant id param in send_out_forms

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -32,12 +32,11 @@ class TemplatesController < ApplicationController
     render 'templates/send/index'
   end
 
-  # FIXME: Move this route to /templates instead of /templates/:id so that we don't get the template id twice
   # POST: Gives students of disciplines access to a form copied from the given template
   def send_out_forms
     return redirect_to root_path unless user_authenticated && admin_user?
 
-    params.permit(:authenticity_token, :commit, :id, :template_id, discipline_ids: [])
+    params.permit(:authenticity_token, :commit, :template_id, discipline_ids: [])
 
     if params[:discipline_ids].nil?
       flash[:error] = 'Nenhuma disciplina foi selecionada para envio'
