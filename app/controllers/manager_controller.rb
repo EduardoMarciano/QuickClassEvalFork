5330ebeddb58af01131ceaa85164945e9cf3039b
VictorFontesCavalcante <ggamingrule@gmail.com>
2024-07-05 17:09:38 -0300

    Manager controller documentation

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,7 +1,14 @@
+# == ManagerController
+#
+# Controls routes for management. 
+#
 class ManagerController < ApplicationController
   include ManagerHelper
   include AuthenticationConcern
 
+  ##
+  # Controls route "/manager" and displays manager functionality if user is authenticated and has admin permissions.
+  #
   def index
     if user_authenticated == true && admin_user? == true
       render 'index'
