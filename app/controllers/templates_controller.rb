80d077d6f5f32882964bc00d6be351731f5f107c
Gabriel Felix <89144990+gabfelix@users.noreply.github.com>
2024-07-05 09:37:02 -0300

    doc: Add comments to form and template controllers

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -5,35 +5,54 @@ class TemplatesController < ApplicationController
   include ManagerHelper
   include AuthenticationConcern
 
+  ##
+  # Displays a list of templates.
+  #
+  # If the user is an admin, all templates are returned.
+  #
+  # @return [void]
   def index
     return redirect_to root_path unless user_authenticated && admin_user?
 
     @templates = Template.all
   end
 
+  ##
+  # Creates a new template.
+  #
+  # @return [void]
   def new
     return redirect_to root_path unless user_authenticated && admin_user?
 
     @template = Template.new
   end
 
+  ##
+  # Shows details of a specific template.
+  #
+  # @param [Integer] id The ID of the template.
+  # @return [void]
   def show
     return redirect_to root_path unless user_authenticated && admin_user?
 
     @template = Template.find(params[:id])
   end
 
+  ##
+  # Creates a new template based on user input.
+  #
+  # @return [void]
   def create
     return redirect_to root_path unless user_authenticated && admin_user?
 
-    if params[:questions].nil? || (params[:questions][0]["label"] == "" || params[:questions][0]["description"] == "")
-      flash[:error] = 'Crie pelo menos uma questão válida.'
-      redirect_to "/templates/new"
+    if params[:questions].nil?
+      flash[:error] = 'Crie pelo menos uma questão.'
+      redirect_to '/templates/new'
     else
       template = Template.create
       params[:questions].each do |question|
         question = question.permit(:label, :description, :type, :format)
-        question[:description] = nil unless question[:description] != ''
+        question[:description] = nil unless question[:description].present?
         new_question(template, question)
       end
       flash[:success] = 'Modelo de formulário criado com sucesso'
@@ -41,6 +60,10 @@ class TemplatesController < ApplicationController
     end
   end
 
+  ##
+  # Destroys a template.
+  #
+  # @return [void]
   def destroy
     return redirect_to root_path unless user_authenticated && admin_user?
 
@@ -51,7 +74,10 @@ class TemplatesController < ApplicationController
     redirect_to templates_path
   end
 
-  # GET: Page for sending a form to students of a discipline
+  ##
+  # Displays a page for sending a form to students of a discipline.
+  #
+  # @return [void]
   def edit_send
     return redirect_to root_path unless user_authenticated && admin_user?
 
@@ -60,7 +86,10 @@ class TemplatesController < ApplicationController
     render 'templates/send/index'
   end
 
-  # POST: Gives students of disciplines access to a form copied from the given template
+  ##
+  # Gives students of disciplines access to a form copied from the given template.
+  #
+  # @return [void]
   def send_out_forms
     return redirect_to root_path unless user_authenticated && admin_user?
 
@@ -72,6 +101,10 @@ class TemplatesController < ApplicationController
 
   private
 
+  ##
+  # Displays a flash message based on form sending results.
+  #
+  # @return [void]
   def form_send_result_flash
     if !params[:discipline_ids] || params[:discipline_ids].empty?
       flash[:error] = 'Nenhuma disciplina foi selecionada para envio'
@@ -80,24 +113,36 @@ class TemplatesController < ApplicationController
     end
   end
 
+  ##
+  # Sends a form to disciplines.
+  #
+  # @param [Array<Integer>] discipline_ids IDs of disciplines.
+  # @param [Integer] template_id ID of the template.
+  # @return [void]
   def send_form_to_disciplines(discipline_ids, template_id)
     discipline_ids.each do |discipline_id|
-      Form.create! template: Template.find(template_id), discipline: Discipline.find(discipline_id)
+      Form.create!(template: Template.find(template_id), discipline: Discipline.find(discipline_id))
     end
   end
 
+  ##
+  # Creates a new question for the template.
+  #
+  # @param [Template] template The template.
+  # @param [Hash] question A hash containing question details.
+  # @return [Object] The created question.
   def new_question(template, question)
     case question[:type]
     when 'MultipleChoiceQuestion'
       return unless question[:format].present?
 
-      created = MultipleChoiceQuestion.create formlike: template,
+      created = MultipleChoiceQuestion.create(formlike: template,
                                               label: question[:label],
                                               description: question[:description],
-                                              format: question[:format]
+                                              format: question[:format])
     when 'TextInputQuestion'
-      created = TextInputQuestion.create formlike: template, label: question[:label],
-                                         description: question[:description]
+      created = TextInputQuestion.create(formlike: template, label: question[:label],
+                                         description: question[:description])
     else
       flash[:error] = 'Tipo de pergunta inválido'
       redirect_to templates_path
